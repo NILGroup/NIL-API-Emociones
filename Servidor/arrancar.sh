b3060e0b38a4515809f2215e61891a3bb411000d
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2019-02-25 19:50:18 +0100

    Empezado Despliegue con mkosi y contenedores

--- Servidor/arrancar.sh
+++ Servidor/arrancar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 Servidor/manage.py runserver
+python3 manage.py runserver
 #Servidor/tests.sh
 
 # Nota: en producción, hay que llamar a manage.py con collectstatic y apuntar el
