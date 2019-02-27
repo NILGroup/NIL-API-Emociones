3290bb440e6c5cb9f30abe5ae8781a602a7cf5bf
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2019-02-27 17:12:09 +0100

    Funcionando en producción, pero las palabras perdidas (debian estar en la bd)

--- Servidor/arrancar.sh
+++ Servidor/arrancar.sh
@@ -2,6 +2,3 @@
 
 python3 manage.py runserver
 #Servidor/tests.sh
-
-# Nota: en producción, hay que llamar a manage.py con collectstatic y apuntar el
-# servidor nginx o lo que sea para que sirva el directorio static.
