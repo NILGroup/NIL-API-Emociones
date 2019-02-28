37ae9e63ca1c0caf9aa79645a005e0ac6e39d158
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2019-02-28 12:18:33 +0100

    Reorganizado el despliegue de nuevo, y fichero install también para usuario local

--- Despliegue/install.sh
+++ Despliegue/install.sh
@@ -1,30 +1,66 @@
 #!/bin/bash
-# Script que instala las dependencias del proyecto
+#
+# Script que instala en una máquina el servidor y lo prepara para ejecutar.
+#
+# Hay dos opciones, instalar con sudo o root, o como usuario normal.
+#
+# ## Instalación como root
+#
+# Se instalan las dependencias necesarias, los ficheros de configuración, y
+# activan los servicios.
+#
+# **¡Atención!** Se desactivan todos los servidores nginx previos, si se quiere
+# compartir la instancia nginx hay que cambiar después la configuración a mano.
+#
+# ## Instalación como usuario local
+#
+# Se instalan con sudo las dependencias de apt, y sin sudo las de python. Se
+# recomienda activar primero un entorno virtual venv o conda. No se instala
+# nginx, por lo que si se quiere usar hay que hacerlo a mano, usar como
+# inspiración el fichero del directorio Despliegue.
 
-# Instalar dependencias de aptitude
-apt install -y python3 python3-pip python3-setuptools \
-    python3-wheel build-essential python3-dev \
-    nginx
+DEPENDENCIAS="python3 python3-pip python3-setuptools python3-wheel build-essential python3-dev nginx"
+PAQUETES_PIP="django djangorestframework requests cython pystemmer spacy"
+
+if [ "$EUID" -eq 0 ]; then
+    # Como root
+    echo "Instalando como ROOT"
+    COMO_ROOT=1
+    PATH_SERVIDOR="/Servidor"
+    apt install -y $DEPENDENCIAS
+    PYTHON=python3
+    PIP=pip3
+else
+    # Usuario local
+    echo "Instalando como usuario local"
+    COMO_ROOT=0
+    PATH_SERVIDOR="$PWD/Servidor"
+    sudo apt install $DEPENDENCIAS
+    PYTHON=python
+    PIP=pip
+fi
 
 # Instalar dependencias python
-pip3 install django \
-            djangorestframework \
-            requests \
-            cython \
-            pystemmer \
-            spacy
+$PIP install $PAQUETES_PIP
 
 # Descargar el modelo de spacy
-python3 -m spacy download es
+$PYTHON -m spacy download es
+
+# Ejecutar migraciones de django y preparar los estáticos
+pushd $PATH_SERVIDOR
+$PYTHON manage.py migrate
+$PYTHON manage.py collectstatic --clear --noinput
+popd
+
+if [ "$COMO_ROOT" -ne 1 ]; then exit 0; fi
 
 # Instalar la configuración para nginx
 rm -f /etc/nginx/sites-enabled/*
-ln -s /etc/nginx/sites-available/emociones /etc/nginx/sites-enabled/emociones
+cp Despliegue/emociones.nginx /etc/nginx/sites-available/emociones.nginx
+ln -s /etc/nginx/sites-available/emociones.nginx /etc/nginx/sites-enabled/emociones.nginx
 
-# Ejecutar migraciones de django y preparar los estáticos
-cd /Servidor
-python3 manage.py migrate
-python3 manage.py collectstatic
+# Instalar el servico de systemd
+cp Despliegue/emociones.service /etc/systemd/system/emociones.service
 
 # Activar los servicios necesarios para que arranquen con el contenedor
 systemctl enable nginx emociones systemd-networkd systemd-resolved
--- README.md
+++ README.md
@@ -1,19 +1,55 @@
 # NIL-API-Emociones
 
-## Requisitos
+Servidor y API de análisis emocional del lenguaje natural.
 
-- python3
-- django
-- djangorestframework
-- requests
-- Cython
-- PyStemmer (pip)
-- spacy (+ modulo 'es')
+## Contenidos
+
+- `Servidor`: código y datos del servidor python+django.
+- `Despliegue`: Ficheros necesarios para el despliegue e instalación.
+- `README.md`: Este fichero.
+ 
+## Uso
+
+### Como proceso local
+
+Crear un entorno virtual, por ejemplo con conda, y ejecutar el script de
+instalación como usuario no root:
+
+```sh
+$ conda create -n emociones
+$ source activate emociones
+$ ./Despliegue/install.sh
+```
+
+Para lanzar el servidor:
+
+```sh
+$ source activate emociones
+$ cd Servidor && ./arrancar.sh
+```
+
+**Nota**: Para este uso, mejor utilizar `DEBUG=True` en `settings.py`.
+
+### Como contenedor
+
+Para crear un contenedor con el servidor de manera automática, instalar la
+herramienta `mkosi` y ejecutar en este directorio:
+
+```sh
+$ sudo mkosi -f --default=Despliegue/mkosi.default
+```
+
+El contenedor se crea por defecto en `/var/lib/machines`, por lo que se puede
+lanzar usando:
+
+```sh
+$ sudo systemd-nspawn -nb -M emociones-contenedor
+```
 
 ## Créditos
 
-Basado en el trabajo de Fin de Grado titulado "Análisis Emocional para la
-Inclusión Digital" y realizado por Elena Kaloyanova Popova, Gema Eugercios
+Desarrollado para el trabajo de Fin de Grado titulado "Análisis Emocional para
+la Inclusión Digital" y realizado por Elena Kaloyanova Popova, Gema Eugercios
 Suárez y Paloma Gutiérrez Merino.
 
 Actualizado por Antonio F. G. Sevilla <afgs@ucm.es>
